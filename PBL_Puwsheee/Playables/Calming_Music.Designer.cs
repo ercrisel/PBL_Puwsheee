2f0812695aefc29445d9d7ddd3b6318e3ff35af2
ashley yulo <ashleyyulo04@yahoo.com>
2020-11-19 01:37:23 +0800

    Breathing Exercise Form

---
+++ PBL_Puwsheee/Playables/Calming_Music.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace PBL_Puwsheee.Playables
+{
+    partial class Calming_Music
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Calming_Music
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(198)))), ((int)(((byte)(187)))));
+            this.ClientSize = new System.Drawing.Size(776, 442);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Calming_Music";
+            this.Text = "Calming_Music";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
