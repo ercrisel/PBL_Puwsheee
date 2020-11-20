f24d56284f83b918881fc851a27533a581c60cb3
Lana Bernardo <lanagabrielle.bernardo@gmail.com>
2020-11-20 18:58:56 +0800

    yes

--- PBL_Puwsheee/Analysis.Designer.cs
+++ PBL_Puwsheee/Analysis.Designer.cs
@@ -54,11 +54,12 @@
             // guna2CirclePictureBox1
             // 
             this.guna2CirclePictureBox1.Image = global::PBL_Puwsheee.Properties.Resources.SamplePhoto;
-            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(182, 111);
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(243, 137);
+            this.guna2CirclePictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
             this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
-            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(107, 98);
             this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox1.TabIndex = 0;
             this.guna2CirclePictureBox1.TabStop = false;
@@ -66,11 +67,12 @@
             // guna2CirclePictureBox2
             // 
             this.guna2CirclePictureBox2.Image = global::PBL_Puwsheee.Properties.Resources.SamplePhoto;
-            this.guna2CirclePictureBox2.Location = new System.Drawing.Point(179, 232);
+            this.guna2CirclePictureBox2.Location = new System.Drawing.Point(239, 286);
+            this.guna2CirclePictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
             this.guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox2.ShadowDecoration.Parent = this.guna2CirclePictureBox2;
-            this.guna2CirclePictureBox2.Size = new System.Drawing.Size(55, 55);
+            this.guna2CirclePictureBox2.Size = new System.Drawing.Size(73, 68);
             this.guna2CirclePictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox2.TabIndex = 1;
             this.guna2CirclePictureBox2.TabStop = false;
@@ -78,11 +80,12 @@
             // guna2CirclePictureBox3
             // 
             this.guna2CirclePictureBox3.Image = global::PBL_Puwsheee.Properties.Resources.SamplePhoto;
-            this.guna2CirclePictureBox3.Location = new System.Drawing.Point(57, 232);
+            this.guna2CirclePictureBox3.Location = new System.Drawing.Point(76, 286);
+            this.guna2CirclePictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox3.Name = "guna2CirclePictureBox3";
             this.guna2CirclePictureBox3.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox3.ShadowDecoration.Parent = this.guna2CirclePictureBox3;
-            this.guna2CirclePictureBox3.Size = new System.Drawing.Size(55, 55);
+            this.guna2CirclePictureBox3.Size = new System.Drawing.Size(73, 68);
             this.guna2CirclePictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox3.TabIndex = 1;
             this.guna2CirclePictureBox3.TabStop = false;
@@ -90,11 +93,12 @@
             // guna2CirclePictureBox4
             // 
             this.guna2CirclePictureBox4.Image = global::PBL_Puwsheee.Properties.Resources.SamplePhoto;
-            this.guna2CirclePictureBox4.Location = new System.Drawing.Point(118, 232);
+            this.guna2CirclePictureBox4.Location = new System.Drawing.Point(157, 286);
+            this.guna2CirclePictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox4.Name = "guna2CirclePictureBox4";
             this.guna2CirclePictureBox4.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox4.ShadowDecoration.Parent = this.guna2CirclePictureBox4;
-            this.guna2CirclePictureBox4.Size = new System.Drawing.Size(55, 55);
+            this.guna2CirclePictureBox4.Size = new System.Drawing.Size(73, 68);
             this.guna2CirclePictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox4.TabIndex = 2;
             this.guna2CirclePictureBox4.TabStop = false;
@@ -102,11 +106,12 @@
             // guna2CirclePictureBox5
             // 
             this.guna2CirclePictureBox5.Image = global::PBL_Puwsheee.Properties.Resources.SamplePhoto;
-            this.guna2CirclePictureBox5.Location = new System.Drawing.Point(240, 232);
+            this.guna2CirclePictureBox5.Location = new System.Drawing.Point(320, 286);
+            this.guna2CirclePictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox5.Name = "guna2CirclePictureBox5";
             this.guna2CirclePictureBox5.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox5.ShadowDecoration.Parent = this.guna2CirclePictureBox5;
-            this.guna2CirclePictureBox5.Size = new System.Drawing.Size(55, 55);
+            this.guna2CirclePictureBox5.Size = new System.Drawing.Size(73, 68);
             this.guna2CirclePictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox5.TabIndex = 3;
             this.guna2CirclePictureBox5.TabStop = false;
@@ -114,11 +119,12 @@
             // guna2CirclePictureBox6
             // 
             this.guna2CirclePictureBox6.Image = global::PBL_Puwsheee.Properties.Resources.SamplePhoto;
-            this.guna2CirclePictureBox6.Location = new System.Drawing.Point(302, 232);
+            this.guna2CirclePictureBox6.Location = new System.Drawing.Point(403, 286);
+            this.guna2CirclePictureBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox6.Name = "guna2CirclePictureBox6";
             this.guna2CirclePictureBox6.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox6.ShadowDecoration.Parent = this.guna2CirclePictureBox6;
-            this.guna2CirclePictureBox6.Size = new System.Drawing.Size(55, 55);
+            this.guna2CirclePictureBox6.Size = new System.Drawing.Size(73, 68);
             this.guna2CirclePictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox6.TabIndex = 4;
             this.guna2CirclePictureBox6.TabStop = false;
@@ -128,9 +134,10 @@
             this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(69, 103);
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(92, 127);
+            this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(42, 19);
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(48, 22);
             this.guna2HtmlLabel1.TabIndex = 5;
             this.guna2HtmlLabel1.Text = "Mood";
             // 
@@ -139,9 +146,10 @@
             this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(69, 201);
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(92, 247);
+            this.guna2HtmlLabel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
-            this.guna2HtmlLabel2.Size = new System.Drawing.Size(184, 19);
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(213, 22);
             this.guna2HtmlLabel2.TabIndex = 6;
             this.guna2HtmlLabel2.Text = "Often Together Activities";
             // 
@@ -154,12 +162,13 @@
             this.startdateDateTimePicker.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
             this.startdateDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Long;
             this.startdateDateTimePicker.HoverState.Parent = this.startdateDateTimePicker;
-            this.startdateDateTimePicker.Location = new System.Drawing.Point(182, 25);
+            this.startdateDateTimePicker.Location = new System.Drawing.Point(243, 31);
+            this.startdateDateTimePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.startdateDateTimePicker.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.startdateDateTimePicker.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.startdateDateTimePicker.Name = "startdateDateTimePicker";
             this.startdateDateTimePicker.ShadowDecoration.Parent = this.startdateDateTimePicker;
-            this.startdateDateTimePicker.Size = new System.Drawing.Size(211, 36);
+            this.startdateDateTimePicker.Size = new System.Drawing.Size(281, 44);
             this.startdateDateTimePicker.TabIndex = 7;
             this.startdateDateTimePicker.Value = new System.DateTime(2020, 11, 14, 17, 57, 24, 907);
             // 
@@ -172,12 +181,13 @@
             this.enddateDateTimePicker.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
             this.enddateDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Long;
             this.enddateDateTimePicker.HoverState.Parent = this.enddateDateTimePicker;
-            this.enddateDateTimePicker.Location = new System.Drawing.Point(409, 25);
+            this.enddateDateTimePicker.Location = new System.Drawing.Point(545, 31);
+            this.enddateDateTimePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.enddateDateTimePicker.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.enddateDateTimePicker.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.enddateDateTimePicker.Name = "enddateDateTimePicker";
             this.enddateDateTimePicker.ShadowDecoration.Parent = this.enddateDateTimePicker;
-            this.enddateDateTimePicker.Size = new System.Drawing.Size(211, 36);
+            this.enddateDateTimePicker.Size = new System.Drawing.Size(281, 44);
             this.enddateDateTimePicker.TabIndex = 8;
             this.enddateDateTimePicker.Value = new System.DateTime(2020, 11, 14, 17, 57, 24, 907);
             // 
@@ -191,10 +201,11 @@
             this.seeresultButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.seeresultButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(198)))), ((int)(((byte)(187)))));
             this.seeresultButton.HoverState.Parent = this.seeresultButton;
-            this.seeresultButton.Location = new System.Drawing.Point(638, 25);
+            this.seeresultButton.Location = new System.Drawing.Point(851, 31);
+            this.seeresultButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.seeresultButton.Name = "seeresultButton";
             this.seeresultButton.ShadowDecoration.Parent = this.seeresultButton;
-            this.seeresultButton.Size = new System.Drawing.Size(87, 36);
+            this.seeresultButton.Size = new System.Drawing.Size(116, 44);
             this.seeresultButton.TabIndex = 9;
             this.seeresultButton.Text = "View Result";
             // 
@@ -210,10 +221,11 @@
             this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.resetButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(81)))), ((int)(((byte)(105)))));
             this.resetButton.HoverState.Parent = this.resetButton;
-            this.resetButton.Location = new System.Drawing.Point(388, 460);
+            this.resetButton.Location = new System.Drawing.Point(517, 566);
+            this.resetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.resetButton.Name = "resetButton";
             this.resetButton.ShadowDecoration.Parent = this.resetButton;
-            this.resetButton.Size = new System.Drawing.Size(91, 37);
+            this.resetButton.Size = new System.Drawing.Size(121, 46);
             this.resetButton.TabIndex = 10;
             this.resetButton.Text = "Reset";
             // 
@@ -222,9 +234,10 @@
             this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
-            this.guna2HtmlLabel3.Location = new System.Drawing.Point(619, 297);
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(825, 366);
+            this.guna2HtmlLabel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
-            this.guna2HtmlLabel3.Size = new System.Drawing.Size(181, 19);
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(213, 22);
             this.guna2HtmlLabel3.TabIndex = 11;
             this.guna2HtmlLabel3.Text = "Score History Line Chart";
             // 
@@ -233,9 +246,10 @@
             this.guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
-            this.guna2HtmlLabel4.Location = new System.Drawing.Point(619, 111);
+            this.guna2HtmlLabel4.Location = new System.Drawing.Point(825, 137);
+            this.guna2HtmlLabel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel4.Name = "guna2HtmlLabel4";
-            this.guna2HtmlLabel4.Size = new System.Drawing.Size(122, 19);
+            this.guna2HtmlLabel4.Size = new System.Drawing.Size(142, 22);
             this.guna2HtmlLabel4.TabIndex = 11;
             this.guna2HtmlLabel4.Text = "Mood Line Chart";
             // 
@@ -244,18 +258,19 @@
             this.guna2HtmlLabel5.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
-            this.guna2HtmlLabel5.Location = new System.Drawing.Point(69, 329);
+            this.guna2HtmlLabel5.Location = new System.Drawing.Point(92, 405);
+            this.guna2HtmlLabel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2HtmlLabel5.Name = "guna2HtmlLabel5";
-            this.guna2HtmlLabel5.Size = new System.Drawing.Size(89, 19);
+            this.guna2HtmlLabel5.Size = new System.Drawing.Size(103, 22);
             this.guna2HtmlLabel5.TabIndex = 15;
             this.guna2HtmlLabel5.Text = "Mood Count";
             // 
             // Analysis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(198)))), ((int)(((byte)(187)))));
-            this.ClientSize = new System.Drawing.Size(936, 514);
+            this.ClientSize = new System.Drawing.Size(1248, 633);
             this.Controls.Add(this.guna2HtmlLabel5);
             this.Controls.Add(this.guna2HtmlLabel4);
             this.Controls.Add(this.guna2HtmlLabel3);
@@ -273,6 +288,7 @@
             this.Controls.Add(this.guna2CirclePictureBox1);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(21)))), ((int)(((byte)(41)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Analysis";
             this.Text = "Analysis";
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
