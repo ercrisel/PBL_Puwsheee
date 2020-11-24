0354e35db574afa0f86e392767d476886a9abe74
ashley yulo <ashleyyulo04@yahoo.com>
2020-11-24 22:21:02 +0800

    fixed nav bar added visualization form

---
+++ PBL_Puwsheee/Visualization/MainVisualization.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL_Puwsheee
+{
+    public partial class MainVisualization : Form
+    {
+        public MainVisualization()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form bg = new Form();
+            using (Form card = new Calendar.Calendar_Card())
+            {
+                    bg.StartPosition = FormStartPosition.CenterScreen;
+                    bg.FormBorderStyle = FormBorderStyle.None;
+                    bg.Opacity = .50d;
+                    bg.BackColor = Color.Black;
+                    bg.WindowState = FormWindowState.Normal;
+                    bg.TopMost = true;
+                    bg.Location = this.Location;
+                    bg.ShowInTaskbar = false;
+                    bg.Size = new Size(1020, 580);
+                    bg.Show();
+
+                    card.Owner = bg;
+                    card.ShowDialog();
+                    bg.Dispose();
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(387, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "visualization";
+            // 
+            // MainVisualization
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(198)))), ((int)(((byte)(187)))));
+            this.ClientSize = new System.Drawing.Size(935, 505);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainVisualization";
+            this.Text = "Visualization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
